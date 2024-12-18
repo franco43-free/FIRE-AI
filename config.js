927401304783f297f8bc077a796beeccb0f538b1
franco43-free <75459430+franco43-free@users.noreply.github.com>
2024-12-18 14:01:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -23,9 +23,9 @@ for (let i = 0; i < ownerlist.length; i += 2) {
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['2349013432338', '2349013432338']
-global.prems = ['2349013432338', '2349013432338', '2349013432338']
-global.allowed = ['2349013432338', '2349013432338', '2349013432338']
+global.mods = ['543844457266', '543844457266']
+global.prems = ['543844457266', '543844457266', '543844457266']
+global.allowed = ['543844457266', '543844457266', '543844457266']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = [
